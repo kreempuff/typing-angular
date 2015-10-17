c2abe737df9b9e636037c14d52fdeb90f5d0be16
lentusignavus <ignavus.ii@gmail.com>
2015-10-16 18:00:46 -0700

    Major overhaul of website

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,22 +1,90 @@
 (function() {
 	'use strict';
-	angular.module('app', ['ui.router',"ngAnimate", "ngMaterial"])
-	.config(Config);
-	Config.$inject = ['$stateProvider', '$urlRouterProvider'];
-	function Config($stateProvider, $urlRouterProvider) {
-		$stateProvider.state('Education', {
-			templateUrl: 'views/education.html',
+	angular.module('app', ["ngAnimate", "ngMaterial"]);
 
-		}).state('Experience',{
-			templateUrl: 'views/experience.html',
+})();
 
-		}).state('About', {
-			templateUrl: 'views/about.html',
+(function() {
+  'use strict';
+  angular.module('app')
+    .controller('MainController', MainController);
 
-		}).state('Portfolio',{
-			templateUrl: 'views/projects.html',
+  MainController.$inject = ['$interval', "$timeout"];
 
-		});
-		$urlRouterProvider.otherwise('/');
-	}
+  function MainController($interval, $timeout) {
+    var vm = this;
+    vm.type = {
+      text: ""
+    };
+    vm.rightString = "I mean: Hi, my name is Kareem.";
+    vm.wrongString = "Hey I'm desperate, please hire me.";
+    vm.secondMessage = "I just became a MEAN Stack Developer.";
+    vm.secondType = [];
+    vm.typingAnimationDone;
+    vm.count = 0;
+    vm.done = false;
+
+
+
+
+
+
+
+
+
+
+
+    // FUNCTION FOR TYPING
+    vm.backspace = function(string, number, funcToRunAfterBackspace) {
+      var stringToBeWorked = string.split("");
+      var backspaceInterval = $interval(function() {
+        if (stringToBeWorked.length === number) {
+          $interval.cancel(backspaceInterval);
+          funcToRunAfterBackspace();
+        }
+
+        stringToBeWorked.splice(stringToBeWorked.length - 1, 1);
+        vm.type.text = stringToBeWorked.join("");
+      }, 50);
+
+    };
+    vm.firstType = function(string, backspacefunc) {
+      var type = $interval(function() {
+        if (vm.type.text === string) {
+          $interval.cancel(type);
+          vm.count = 0;
+          backspacefunc(vm.type.text, 0, function() {
+            vm.firstType(vm.rightString, function() {
+              vm.MEANtype();
+            });
+          });
+
+          // vm.SecondType();
+
+          return;
+        }
+        vm.type.text += string[vm.count];
+        vm.count += 1;
+      }, 70);
+    };
+
+    vm.MEANtype = function() {
+      var secondType = $interval(function() {
+        if (vm.count === vm.secondMessage.length) {
+          $interval.cancel(secondType);
+          $timeout(function() {
+            vm.typingAnimationDone = true;
+            vm.done = true;
+          }, 500);
+        }
+        vm.secondType.push(vm.secondMessage[vm.count]);
+        vm.count += 1;
+      }, 50);
+    };
+
+
+
+
+    vm.firstType(vm.wrongString, vm.backspace);
+  }
 })();
